fbf69cdc4c1f8674e4e0cc13094b6d95513c52c5
Ruken Karanci <95680064+Karanci@users.noreply.github.com>
2025-06-30 11:06:49 +0300

    Update config.py

--- server/dist/config.py
+++ server/dist/config.py
@@ -1,10 +1,10 @@
 # Veritabanı ayarları
 DB_CONFIG = {
-    'host': 'mysql-20847602-ktun-f5b8.h.aivencloud.com',
-    'user': 'avnadmin',
-    'password': 'AVNS_J3Mb3cvEjKmRdkQc1DN',
+    'host': 'host',
+    'user': 'admin',
+    'password': 'admin',
     'database': 'defaultdb',
-    'port': 13447,
+    'port': 13345,
     'charset': 'utf8mb4',
     'ssl_verify_cert': False,  # Client PC'lerde sertifika doğrulamasını devre dışı bırakıyoruz
     'ssl_ca': None             # Client PC'lerde SSL sertifikası kullanmıyoruz
